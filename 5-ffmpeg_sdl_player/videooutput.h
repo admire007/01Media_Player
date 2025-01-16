cf3aeefa3a83769e67e10b2164e097340959a207
一夜奈良川 <2606865605@qq.com>
2025-01-16 21:06:24 +0800

    7.无音视频同步可执行版

---
+++ 5-ffmpeg_sdl_player/videooutput.h
@@ -0,0 +1,41 @@
+#ifndef VIDEOOUTPUT_H
+#define VIDEOOUTPUT_H
+
+#ifdef __cplusplus  ///
+extern "C"
+{
+// 包含ffmpeg头文件
+//#include "libavutil/avutil.h"
+#include "SDL.h"
+}
+#endif
+
+
+#include "avframequeue.h"
+
+
+class VideoOutput
+{
+public:
+    VideoOutput(AVFrameQueue *frame_queue, int video_width, int video_height);
+    int Init();
+    int MainLoop();
+    void RefreshLoopWaitEvent(SDL_Event *event);
+private:
+    void videoRefresh(double *remaining_time);
+    AVFrameQueue *frame_queue_ = NULL;
+    SDL_Event event_; // 事件
+    SDL_Rect rect_;
+    SDL_Window *win_ = NULL;
+    SDL_Renderer *renderer_ = NULL;
+    SDL_Texture *texture_ = NULL;
+
+    int video_width_ = 0;
+    int video_height_ = 0;
+    uint8_t *yuv_buf_ = NULL;
+    int      yuv_buf_size_ = 0;
+
+//    SDL_mutex mutex_;
+};
+
+#endif // VIDEOOUTPUT_H
